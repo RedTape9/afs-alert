51c8880e1d3c6d6f9c7d091e50b320ed4575b0e8
RedTape <redtape9688@gmail.com>
2023-10-14 12:52:58 +0200

    Button, Input changed to react-native-elements package

--- screens/ResetScreen.js
+++ screens/ResetScreen.js
@@ -1,118 +1,64 @@
+import React, { useState } from 'react';
+import { Alert, View, Image, StyleSheet } from 'react-native';
 import { useNavigation } from "@react-navigation/core";
-import React, { useState } from "react";
-import {
-    KeyboardAvoidingView,
-    StyleSheet,
-    Text,
-    TextInput,
-    TouchableOpacity,
-    View,
-    Alert,
-    Image,
-} from "react-native";
+import { Button, Input } from 'react-native-elements';
 import { firebase } from "../firebase";
 
 const ResetScreen = () => {
-
-
-    const navigation = useNavigation();
     const [email, setEmail] = useState('');
+    const navigation = useNavigation();
 
     const onResetPasswordPress = () => {
         firebase.auth().sendPasswordResetEmail(email)
             .then(() => {
-                Alert.alert("Sie erhalten in Kürze eine E-Mail. Folgen Sie bitte den Anweisungen.");
+                Alert.alert("Passwort-Reset-E-Mail wurde gesendet!");
             }, (error) => {
-                Alert.alert("Die eingegebene E-Mail Adresse ist uns unbekannt. ")
+                Alert.alert(error.message);
             });
     }
 
     const goBackToLogin = () => {
-        navigation.replace("Login");
-    };
+        navigation.goBack();
+    }
 
     return (
-        <KeyboardAvoidingView style={styles.container} behavior="padding" >
+        <View style={styles.container}>
             <Image source={require('../images/logo_small.webp')} style={styles.logo} />
-            <View style={styles.inputContainer}>
-                <TextInput
-                    style={styles.input}
-                    placeholder='E-mail'
-                    placeholderTextColor="#a9d0fc"
-                    onChangeText={(text) => setEmail(text)}
-                    value={email}
-                />
-            </View>
-
-            <View style={styles.buttonContainer}>
-                <TouchableOpacity
-                    onPress={onResetPasswordPress}
-                    style={styles.button}
-                >
-                    <Text style={styles.buttonText}>Passwort zurücksetzen</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    onPress={goBackToLogin}
-                    style={[styles.button, styles.buttonOutline]}
-                >
-                    <Text style={styles.buttonOutlineText}>zurück</Text>
-                </TouchableOpacity>
-            </View>
-        </KeyboardAvoidingView >
+            <Input
+                placeholder="E-mail"
+                value={email}
+                onChangeText={(text) => setEmail(text)}
+            />
+            <Button title="Passwort zurücksetzen" onPress={onResetPasswordPress} containerStyle={styles.buttonContainer} buttonStyle={styles.button} />
+            <Button title="Zurück zum Login" onPress={goBackToLogin} type="clear" titleStyle={styles.footerLink} />
+        </View>
     );
 };
-
-
-export default ResetScreen;
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    inputContainer: {
-        width: "80%",
-    },
-    input: {
-        backgroundColor: "white",
-        paddingHorizontal: 15,
-        paddingVertical: 10,
-        borderRadius: 10,
-        marginTop: 5,
-    },
-    buttonContainer: {
-        width: "60%",
-        justifyContent: "center",
-        alignItems: "center",
-        marginTop: 40,
-    },
-    button: {
-        backgroundColor: "#007ac5",
-        width: "100%",
-        padding: 15,
-        borderRadius: 10,
-        alignItems: "center",
-    },
-    buttonOutline: {
-        backgroundColor: "white",
-        marginTop: 5,
-        borderColor: "#007ac5",
-        borderWidth: 2,
-    },
-    buttonText: {
-        color: "white",
-        fontWeight: "700",
-        fontSize: 16,
-    },
-    buttonOutlineText: {
-        color: "#007ac5",
-        fontWeight: "700",
-        fontSize: 16,
+        justifyContent: 'center',
+        alignItems: 'center',
     },
     logo: {
-        marginBottom: 40,
+        marginBottom: 20,
         width: 200,
         height: 140,
     },
+    buttonContainer: {
+        width: "60%",
+        marginTop: 20,
+    },
+    button: {
+        backgroundColor: "#007ac5",
+        borderRadius: 10,
+    },
+    footerLink: {
+        color: "#007ac5",
+        fontWeight: "bold",
+        fontSize: 16,
+    }
 });
+
+export default ResetScreen;
