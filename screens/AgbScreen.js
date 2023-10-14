be89db49df0df81d5c22e0bfbc76e1261cae4959
RedTape <redtape9688@gmail.com>
2023-10-14 12:46:40 +0200

    react-native-paper CheckBox replaced with one from react-native-elements

--- screens/AgbScreen.js
+++ screens/AgbScreen.js
@@ -1,70 +1,41 @@
+import React, { useState } from 'react';
 import { useNavigation } from "@react-navigation/core";
-import React from "react";
-import { Checkbox } from 'react-native-paper';
-import {
-    KeyboardAvoidingView,
-    StyleSheet,
-    Text,
-    TouchableOpacity,
-    View,
-    Image,
-    Platform,
-    ScrollView,
-    Alert
-} from "react-native";
+import { Alert, KeyboardAvoidingView, StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+import { Button, CheckBox } from 'react-native-elements';
 
 const AgbScreen = () => {
+    const [checked, setChecked] = useState(false);
     const navigation = useNavigation();
-
-    const [checked, setChecked] = React.useState(false);
 
     const handleSignUp = () => {
         if (checked) {
             navigation.replace("SignIn");
-        }
-        else {
+        } else {
             Alert.alert("Bitte bestätigen Sie zuerst unsere AGBs.");
         }
-
-
     };
-
 
     return (
         <KeyboardAvoidingView style={styles.container} behavior="padding">
             <Image source={require('../images/logo_small.webp')} style={styles.logo} />
-            <View style={styles.inputContainer}>
-                <ScrollView style={{ height: 300, borderRadius: 10, }}>
-                    <Text style={styles.agb}> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non tellus orci ac auctor augue mauris augue neque gravida. Sit amet nulla facilisi morbi tempus. Ultricies tristique nulla aliquet enim tortor. Purus gravida quis blandit turpis cursus in hac. Pellentesque dignissim enim sit amet venenatis urna cursus. Lectus sit amet est placerat in egestas. Erat pellentesque adipiscing commodo elit at imperdiet dui accumsan. Id semper risus in hendrerit gravida rutrum quisque non. Neque convallis a cras semper auctor. Pretium vulputate sapien nec sagittis aliquam malesuada bibendum arcu. Senectus et netus et malesuada fames ac turpis. Malesuada nunc vel risus commodo viverra maecenas accumsan lacus vel. Vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant morbi. Dolor sit amet consectetur adipiscing elit duis tristique sollicitudin. Eleifend donec pretium vulputate sapien nec. Amet mattis vulputate enim nulla aliquet porttitor lacus. Hendrerit gravida rutrum quisque non tellus orci ac auctor augue. Sed felis eget velit aliquet sagittis id.
-                    </Text>
-                </ScrollView>
+            <ScrollView style={styles.agb}>
+                <Text>
+                    Lorem ipsum... [AGBs]
+                </Text>
+            </ScrollView>
+            <View style={styles.checkboxContainer}>
+                <CheckBox
+                    title="Ich stimme den AGBs zu"
+                    checked={checked}
+                    onPress={() => setChecked(!checked)}
+                    containerStyle={styles.checkbox}
+                    checkedColor="#007ac5"
+                />
             </View>
-
-            <View style={{ flexDirection: "row" }}>
-                <View style={styles.checkboxContainer}>
-                    <Checkbox style={styles.checkbox}
-                        status={checked ? 'checked' : 'unchecked'} color={checked ? "#007ac5" : "red"}
-                        onPress={() => {
-                            setChecked(!checked);
-                        }}
-                    />
-                </View>
-                <Text style={styles.label}>AGBs gelesen und einverstanden.</Text>
-            </View>
-
-            <View style={styles.buttonContainer}>
-                <TouchableOpacity
-                    onPress={handleSignUp}
-                    style={[styles.button, styles.buttonOutline]}
-                >
-                    <Text style={styles.buttonOutlineText}>Registrieren</Text>
-                </TouchableOpacity>
-            </View>
+            <Button title="Registrieren" onPress={handleSignUp} containerStyle={styles.buttonContainer} buttonStyle={styles.button} />
         </KeyboardAvoidingView>
     );
 };
-
-export default AgbScreen;
 
 const styles = StyleSheet.create({
     container: {
@@ -72,79 +43,39 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         alignItems: "center",
     },
-    inputContainer: {
-        width: "80%",
-    },
-    input: {
-        backgroundColor: "white",
-        paddingHorizontal: 15,
-        paddingVertical: 10,
-        borderRadius: 10,
-        marginTop: 5,
-    },
-    buttonContainer: {
-        width: "60%",
-        justifyContent: "center",
-        alignItems: "center",
-        marginTop: 10,
-    },
-    button: {
-        backgroundColor: "#007ac5",
-        width: "100%",
-        padding: 15,
-        borderRadius: 10,
-        alignItems: "center",
-    },
-    buttonOutline: {
-        backgroundColor: "white",
-        marginTop: 5,
-        borderColor: "#007ac5",
-        borderWidth: 1,
-    },
-    buttonText: {
-        color: "white",
-        fontWeight: "700",
-        fontSize: 16,
-    },
-    buttonOutlineText: {
-        color: "#007ac5",
-        fontWeight: "700",
-        fontSize: 16,
-    },
-    moreText: {
-        color: "#007ac5",
-        fontWeight: "700",
-        fontSize: 16,
-        marginTop: 20,
-    },
     logo: {
         alignSelf: "center",
         marginTop: 40,
         marginBottom: 20,
         width: 100,
         height: 70,
-
     },
     agb: {
+        width: "80%",
         backgroundColor: "white",
         paddingHorizontal: 15,
         paddingVertical: 5,
         borderRadius: 10,
+        marginBottom: 10,
     },
     checkboxContainer: {
         flexDirection: "row",
+        alignItems: "center",
         marginTop: 10,
-        borderWidth: 1,
-        borderColor: Platform.OS === 'ios' ? "#007ac5" : 0,
-        borderRadius: 3,
     },
     checkbox: {
-        alignSelf: "center",
-
+        backgroundColor: 'transparent',
+        borderWidth: 0,
+        padding: 0,
     },
-    label: {
+    buttonContainer: {
+        width: "60%",
         marginTop: 20,
-        marginLeft: 10,
+    },
+    button: {
+        backgroundColor: "#007ac5",
+        borderRadius: 10,
     },
 });
 
+export default AgbScreen;
